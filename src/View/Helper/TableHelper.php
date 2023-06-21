b5e9e10bb96757548e691553cd48ba8785a37cfa
Fernando Herrero <ininflamable@gmail.com>
2023-06-22 00:44:59 +0200

    Update sources, callbacks

--- src/View/Helper/TableHelper.php
+++ src/View/Helper/TableHelper.php
@@ -1,31 +1,31 @@
 <?php
-declare(strict_types=1);
+	declare(strict_types=1);
 
-namespace DataTables\View\Helper;
+	namespace DataTables\View\Helper;
 
-use Cake\View\Helper;
-use Cake\View\Helper\HtmlHelper;
-use Cake\View\View;
-use DataTables\DataTablesPlugin;
+	use Cake\View\Helper;
+	use Cake\View\Helper\HtmlHelper;
+	use Cake\View\View;
+	use DataTables\DataTablesPlugin;
 
-/**
- * Table helper
- */
-class TableHelper extends HtmlHelper
-{
-    /**
-     * Default configuration.
-     *
-     * @var array<string, mixed>
-     */
-    //protected $_defaultConfig = [];
+	/**
+	 * Table helper
+	 */
+	class TableHelper extends HtmlHelper
+	{
+		/**
+		 * Default configuration.
+		 *
+		 * @var array<string, mixed>
+		 */
+		//protected $_defaultConfig = [];
 
 		public function getTags ($block = null, string $theme = 'DataTables.dataTables.bootstrap') : string {
 			$out = '';
 
-			$out .= $this->script('https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.21/js/jquery.dataTables.min.js', ['block' => $block, 'integrity' => 'sha512-BkpSL20WETFylMrcirBahHfSnY++H2O1W+UnEEO4yNIl+jI2+zowyoGJpbtk6bx97fBXf++WJHSSK2MV4ghPcg==', 'crossorigin' => 'anonymous', 'referrerpolicy' => 'no-referrer']);
-			$out .= $this->script('https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.21/js/dataTables.bootstrap.min.js', ['block' => $block, 'integrity' => 'sha512-F0E+jKGaUC90odiinxkfeS3zm9uUT1/lpusNtgXboaMdA3QFMUez0pBmAeXGXtGxoGZg3bLmrkSkbK1quua4/Q==', 'crossorigin' => 'anonymous', 'referrerpolicy' => 'no-referrer']);
-			$out .= $this->css('https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.21/css/dataTables.bootstrap.min.css', ['block' => $block, 'integrity' => 'sha512-BMbq2It2D3J17/C7aRklzOODG1IQ3+MHw3ifzBHMBwGO/0yUqYmsStgBjI0z5EYlaDEFnvYV7gNYdD3vFLRKsA==', 'crossorigin' => 'anonymous', 'referrerpolicy' => 'no-referrer']);
+			$out .= $this->script('https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js', ['block' => $block]);
+			$out .= $this->script('https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap.min.js', ['block' => $block]);
+			$out .= $this->css('https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap.min.css', ['block' => $block,]);
 			$out .= $this->css($theme ?? 'DataTables.dataTables.bootstrap', ['block' => $block]);
 
 			return $out;
@@ -38,14 +38,21 @@ class TableHelper extends HtmlHelper
 			];
 
 			if (!empty($options['language']) and is_string($options['language'])) {
-				if (!preg_match('~^\{.*\}$~', $options['language']) and is_file(DataTablesPlugin::I18N . $options['language'] . '.json')) {
-					$options['language'] = file_get_contents(DataTablesPlugin::I18N . $options['language'] . '.json');
-					$options['language'] = json_decode($options['language'], true);
-				}
+					if (!preg_match('~^\{.*\}$~', $options['language']) and is_file(DataTablesPlugin::I18N . $options['language'] . '.json')) {
+							$options['language'] = file_get_contents(DataTablesPlugin::I18N . $options['language'] . '.json');
+							$options['language'] = json_decode($options['language'], true);
+					}
 			}
 
+			if (isset($options['footerCallback'])) {
+					$options['footerCallback'] = preg_replace('~[\r\n\t]~', '', $options['footerCallback']);
+			}
 			$options = json_encode($options);
+
+			$options = preg_replace('~("initComplete":)"([^\"]+)"~', '$1$2', $options);
+			$options = preg_replace('~("createdRow":)"([^\"]+)"~', '$1$2', $options);
+			$options = preg_replace('~("footerCallback":)"([^\"]+)"~', '$1$2', $options);
 
 			return sprintf($template, $selector, $options);
 		}
-}
+	}
